b6791f25d642733f9c002ed2dbc61f004010b11c
Lovenoreus <31434044+Lovenoreus@users.noreply.github.com>
2022-03-06 22:16:54 +0100

    done with models

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import User , AbstractUser
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique = True)
+    birth_date = models.DateField(null=True)
+    phone_number = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.username
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    text = models.CharField(max_length = 255 * 4)
+    made_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    posted = models.DateField(default = timezone.now)
+
+    def __str__(self):
+            return self.title
--- activities/views.py
+++ activities/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'activities/home.html') 
+    posts = Post.objects.all()
+    return render(request, 'activities/home.html', {'posts': posts}) 
 
 def about(request):
     return render(request, 'activities/about.html') 
+
+def signup_user(request):
+    if request.method == "GET":
+        return render(request, "activities/signup.html")
+    else:
+        pass
